c0a0508b20ea2d8f9b46126dc9763fd7ae3eb95c
mingo <zzmingo@qq.com>
2020-01-18 18:09:21 +0800

    feat: recreate ios structure

---
+++ ios/AppShellBridgeWebView/AppShellBridgeWebView/AppShellBridgeWebView.h
@@ -0,0 +1,19 @@
+//
+//  AppShellBridgeWebView.h
+//  AppShellBridgeWebView
+//
+//  Created by mingo on 2020/1/18.
+//  Copyright © 2020 appshell.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppShellBridgeWebView.
+FOUNDATION_EXPORT double AppShellBridgeWebViewVersionNumber;
+
+//! Project version string for AppShellBridgeWebView.
+FOUNDATION_EXPORT const unsigned char AppShellBridgeWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppShellBridgeWebView/PublicHeader.h>
+
+
