469672fc068a5a48881a39791dc643b1cfdab904
mingo <zzmingo@qq.com>
2020-01-20 00:31:09 +0800

    fix: react main package

--- android/app/src/main/java/io/appshell/bridge/MainApplication.kt
+++ android/app/src/main/java/io/appshell/bridge/MainApplication.kt
@@ -4,10 +4,34 @@ import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.shell.MainReactPackage
 import com.facebook.soloader.SoLoader
 
 
-class MainApplication : Application() {
+class MainApplication : Application(), ReactApplication {
+
+    private val mReactNativeHost = object : ReactNativeHost(this) {
+        
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            return mutableListOf(
+                MainReactPackage(null),
+                AppShellPackage()
+            )
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
 
     override fun onCreate() {
         super.onCreate()
